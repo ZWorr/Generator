6ce7038644b6c173cafba412b28606ac7736c1ca
Zach Orr <zorr@MacBook-Air-247.local>
2023-12-08 15:52:59 -0700

    Testing github fork

---
+++ src/Physics/BoostedDarkMatter/XSection/FKRDM.h
@@ -0,0 +1,62 @@
+//____________________________________________________________________________
+/*
+Dark Matter model parameters calculated in the same form as
+the Feynmann-Kislinger-Ravndall (FKR) baryon excitation model parameters. (header)
+
+"Physics/Resonance/XSection/FKR.h" -> "Physics/Resonance/XSection/FKRDM.h"
+
+Within FKRDM.h make the following changes from FKR.h:
+  _FKR_H_ -> _FKRDM_H_
+  "FKR" -> "FKRDM"
+  C -> Cs and Cz
+  B -> Bs and Bz
+
+  (Note: the DM calculation does not
+  currently use variables {Rplus,Rminus,Tplus,Tminus,R,T}.
+
+Zachary W. Orr, Colorado State University
+*/
+//____________________________________________________________________________
+
+#ifndef _FKRDM_H_
+#define _FKRDM_H_
+
+#include <iostream>
+
+using std::ostream;
+
+namespace genie {
+
+class FKRDM;
+ostream & operator<< (ostream & stream, const FKRDM & parameters);
+
+class FKRDM {
+
+public:
+
+  friend ostream & operator<< (ostream & stream, const FKRDM & parameters);
+
+  //Unchanged variables
+  double Lamda;
+  double Tv;
+  double Rv;
+  double S;
+  double Ta;
+  double Ra;
+
+  //New variables for DM case that differ from the FKR calculation
+  double Bs;
+  double Cs;
+  double Bz;
+  double Cz;
+
+  void Reset (void);
+  void Print (ostream & stream) const;
+
+  FKRDM();
+  ~FKRDM();
+};
+
+}        // genie namespace
+
+#endif   // _FKRDM_H_
