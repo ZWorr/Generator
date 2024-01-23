7f1bcf28afb1517a445d94b74bb155ad27de08cd
Zach Orr <zorr@MacBook-Air-345.local>
2024-01-23 13:39:31 -0700

    Updated Res DM

--- src/Physics/BoostedDarkMatter/XSection/RSHelicityAmplModelDMn.h
+++ src/Physics/BoostedDarkMatter/XSection/RSHelicityAmplModelDMn.h
@@ -2,17 +2,6 @@
 /*
 Dark Matter Resonant Scattering Amplitude Calculation for neutron Interactions (header)
 
-Taken from the neutrino case for EM interactions:
-  "Physics/Resonance/XSection/RSHelicityAmplModelEMn.h" -> "Physics/Resonance/XSection/RSHelicityAmplModelDMn.h"
-
-Within RSHelicityAmplModelDMp.h make the changes:
-  #include "Physics/Resonance/XSection/RSHelicityAmplModelI.h" -> #include "Physics/Resonance/XSection/RSHelicityAmplModelDMI.h"
-  Change all "EM" to "DM"
-  RSHelicityAmplModelDMI interface implementation: add "DM" to all RSHelicityAmpl
-  FKR -> FKRDM and fkr -> fkrdm
-  ? fAmpl -> fAmplDM ?
-
-
 Zachary W. Orr, Colorado State University
 */
 //____________________________________________________________________________
@@ -21,7 +10,7 @@ Zachary W. Orr, Colorado State University
 #ifndef _HELICITY_AMPL_MODEL_DM_N_H_
 #define _HELICITY_AMPL_MODEL_DM_N_H_
 
-#include "Physics/Resonance/XSection/RSHelicityAmplModelDMI.h" //Specific to DM
+#include "Physics/BoostedDarkMatter/XSection/RSHelicityAmplModelDMI.h"
 
 namespace genie {
 
@@ -32,7 +21,6 @@ public:
   RSHelicityAmplModelDMn(string config);
   virtual ~RSHelicityAmplModelDMn();
 
-  // RSHelicityAmplModelDMI interface implementation
   const RSHelicityAmplDM & Compute(Resonance_t res, const FKRDM & fkrdm) const;
 
 private:
