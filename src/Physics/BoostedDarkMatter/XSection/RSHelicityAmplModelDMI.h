12d53d83a6c543983f7964c93c00b310b349f368
Zach Orr <zorr@MacBook-Air-345.local>
2024-01-23 13:40:04 -0700

    Updated Res DM

--- src/Physics/BoostedDarkMatter/XSection/RSHelicityAmplModelDMI.h
+++ src/Physics/BoostedDarkMatter/XSection/RSHelicityAmplModelDMI.h
@@ -2,18 +2,6 @@
 /*
 RSHelicityAmplModelDMI: the interface for the Dark Matter helicity Amplitudes (header)
 
-"Physics/Resonance/XSection/RSHelicityAmplModelI.h" -> "Physics/Resonance/XSection/RSHelicityAmplModelDMI.h"
-
-Within RSHelicityAmplModelDMI.h make the following changes from RSHelicityAmplModelI.h:
-  #include "Physics/Resonance/XSection/FKR.h" -> #include "Physics/Resonance/XSection/FKRDM.h"
-  #include "Physics/Resonance/XSection/RSHelicityAmpl.h" -> #include "Physics/Resonance/XSection/RSHelicityAmplDM.h"
-  _REIN_SEHGAL_HELICITY_AMPL_MODEL_I_H_ -> _REIN_SEHGAL_HELICITY_AMPL_MODEL_DM_I_H_
-  RSHelicityAmplModelI -> RSHelicityAmplModelDMI
-  RSHelicityAmpl -> RSHelicityAmplDM
-  FKR -> FKRDM and fkr -> fkrdm
-
-
-
 Zachary W. Orr, Colorado State University
 */
 //____________________________________________________________________________
@@ -21,10 +9,10 @@ Zachary W. Orr, Colorado State University
 #ifndef _REIN_SEHGAL_HELICITY_AMPL_MODEL_DM_I_H_
 #define _REIN_SEHGAL_HELICITY_AMPL_MODEL_DM_I_H_
 
-#include "Framework/Algorithm/Algorithm.h" //No change?
-#include "Framework/ParticleData/BaryonResonance.h" //No change?
-#include "Physics/Resonance/XSection/FKRDM.h" //Specific to DM
-#include "Physics/Resonance/XSection/RSHelicityAmplDM.h" //Specific to DM
+#include "Framework/Algorithm/Algorithm.h"
+#include "Framework/ParticleData/BaryonResonance.h"
+#include "Physics/BoostedDarkMatter/XSection/FKRDM.h"
+#include "Physics/BoostedDarkMatter/XSection/RSHelicityAmplDM.h"
 
 namespace genie {
 
@@ -32,31 +20,29 @@ class RSHelicityAmplModelDMI : public Algorithm
 {
 public:
 //______________________ added for quark charges
-    Configure(string config);
+  void Configure(string config);
 //______________________
 
   virtual ~RSHelicityAmplModelDMI();
 
-  // define the RSHelicityAmplModelI interface
   virtual const RSHelicityAmplDM & Compute(Resonance_t res, const FKRDM & fkrdm) const = 0;
 
-
-
-//______________added for quark charges
-private:
-  LoadConfig(void);
-
-  double fQuV;
-  double fQuA;
-  double fQdV;
-  double fQdA;
-//________________
 
 
 protected:
   RSHelicityAmplModelDMI();
   RSHelicityAmplModelDMI(string name);
   RSHelicityAmplModelDMI(string name, string config);
+
+  //______________added for quark charges
+    void LoadConfig(void);
+
+    double fQuV;
+    double fQuA;
+    double fQdV;
+    double fQdA;
+  //________________
+
 };
 
 }        // namespace
